a8421f29a611830000d2f2b55a0bff5d14fe96e2
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-05-30 14:09:39 +0800

    practice with classes

---
+++ test/JS/index.js
@@ -0,0 +1,8 @@
+/* index.js */
+import User from "./user.js";
+
+// Class usage
+let user1 = new User("Sam", "Doe");
+console.log("------");
+let user2 = new User("Charlie", "Blue");
+console.log("=======");
---
+++ test/JS/user.js
@@ -0,0 +1,10 @@
+/* user.js */
+
+// Class definition
+export default class User {
+    constructor(firstName, lastName) {
+      console.log("Creating a new instance of User");
+      console.log(firstName);
+      console.log(lastName);
+    }
+  }
