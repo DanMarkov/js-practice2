f7aeb700c73a44c65a58270fe565dd6bd1a2f3e9
Daniil Markov <alisasuperdetka.kqecn@dralias.com>
2022-05-30 14:06:42 +0800

    classes practice

---
+++ getFullName/JS/user.js
@@ -0,0 +1,14 @@
+/* user.js */
+
+// Class definition
+export default class User {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+  
+  }
